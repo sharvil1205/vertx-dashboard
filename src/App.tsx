01fee87e8df25cfd9a6addbd3cb041832b4de29a
Sharvil Patil <79535235+sharvil1205@users.noreply.github.com>
2025-04-05 22:17:59 +0530

    Fluent UI setup (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
-import "./App.css";
-
 const App = () => {
-  return (
-    <div className="flex justify-center items-center text-4xl">Vertx Labs</div>
-  );
+  return <div>Vertx Labs</div>;
 };
 
 export default App;
