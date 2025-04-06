7984a9033babcba4348849491f7c1e0d04a46de4
Sharvil Patil <79535235+sharvil1205@users.noreply.github.com>
2025-04-06 13:14:08 +0530

    Routing configuration added (#4)

--- src/containers/SidebarContainer.tsx
+++ src/containers/SidebarContainer.tsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import { Sidebar } from "../components";
+import { useNavigate } from "react-router-dom";
 
 const SidebarContainer = () => {
   const [selectedItem, setSelectedItem] = useState<string>("Analytics");
+  const navigate = useNavigate();
   const handleItemClick = (item: string) => {
     setSelectedItem(item);
+    navigate(item.toLowerCase());
   };
   return (
     <Sidebar selectedItem={selectedItem} handleItemClick={handleItemClick} />
